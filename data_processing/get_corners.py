3f1d9aeb0cd64ae886d3287fe2e927c5a6303ac6
Soumya Vadlamannati <vadlamannati1997@gmail.com>
2020-04-14 21:42:43 -0400

    added labels

--- data_processing/get_corners.py
+++ data_processing/get_corners.py
@@ -12,21 +12,28 @@ def get_stats():
 
 	for scene in tqdm(data):
 		corners_list = []
+		label_list = []
 		objects = scene[10]
 		num_objs = objects.shape[1]
 		if num_objs == 0 or num_objs > cfg['MAX_NUM'] or num_objs < cfg['MIN_NUM']:
 			continue
 		objects = objects.squeeze(0) # num_objs
+
 		for obj in objects:
 			basis = obj[0] * obj[1].T
 			corner_1 = obj[2] + basis[0] / 2 + basis[1] / 2
 			corner_2 = obj[2] - basis[0] / 2 + basis[1] / 2
 			corner_3 = obj[2] - basis[0] / 2 - basis[1] / 2
 			corner_4 = obj[2] + basis[0] / 2 - basis[1] / 2
+			
+			label = obj[3][0]
 			corners = np.vstack([corner_1, corner_2, corner_3, corner_4])
 			corners_list.append(corners)
-		corners_list = np.stack(corners_list)    
-		img_corner_list.append(corners_list)
+			label_list.append(label)
+
+		corners_list = np.stack(corners_list)
+		label_list = np.stack(label_list)    
+		img_corner_list.append({'vertices' : corners_list, 'labels' : label_list})
 	import pdb; pdb.set_trace()
 	return img_corner_list
 

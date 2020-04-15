2989fc11f9777ca2a3c03f2751f41eeaa9e27c61
Amala Deshmukh <amala.d166@gmail.com>
2020-04-14 21:44:48 -0400

    Removed pdb

--- data_processing/get_corners.py
+++ data_processing/get_corners.py
@@ -26,15 +26,14 @@ def get_stats():
 			corner_3 = obj[2] - basis[0] / 2 - basis[1] / 2
 			corner_4 = obj[2] + basis[0] / 2 - basis[1] / 2
 			
-			label = obj[3][0]
 			corners = np.vstack([corner_1, corner_2, corner_3, corner_4])
 			corners_list.append(corners)
+			label = obj[3][0]
 			label_list.append(label)
 
 		corners_list = np.stack(corners_list)
 		label_list = np.stack(label_list)    
 		img_corner_list.append({'vertices' : corners_list, 'labels' : label_list})
-	import pdb; pdb.set_trace()
 	return img_corner_list
 
 	

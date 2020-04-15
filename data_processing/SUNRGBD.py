827fe02bd2835c18bab98121c9829433f3b27ada
Bhuvan Agrawal <bhuvan002@gmail.com>
2020-04-14 22:31:24 -0400

    Modify database class

--- data_processing/SUNRGBD.py
+++ data_processing/SUNRGBD.py
@@ -37,6 +37,8 @@ class SUNRGBD:
         for scene in tqdm(self.data):
             corners_list = []
             label_list = []
+            area_list = []
+            
             objects = scene[10]
             num_objs = objects.shape[1]
             if num_objs == 0 or num_objs > cfg['MAX_NUM'] or num_objs < cfg['MIN_NUM']:
@@ -50,14 +52,13 @@ class SUNRGBD:
                 corner_3 = obj[2] - basis[0] / 2 - basis[1] / 2
                 corner_4 = obj[2] + basis[0] / 2 - basis[1] / 2
                 
-                label = obj[3][0]
-                corners = np.vstack([corner_1, corner_2, corner_3, corner_4])
-                corners_list.append(corners)
-                label_list.append(label)
+                corners_list.append(np.vstack([corner_1, corner_2, corner_3, corner_4]))
+                label_list.append(obj[3][0])
+                area_list.append(np.linalg.norm(basis[0]) * np.linalg.norm(basis[1]))
 
             corners_list = np.stack(corners_list)
             label_list = np.stack(label_list)    
-            img_corner_list.append({'vertices' : corners_list, 'labels' : label_list})
+            img_corner_list.append({'vertices' : corners_list, 'labels' : label_list, "areas": area_list})
         if not os.path.exists(cache_dir):
             os.mkdir(cache_dir)
         pickle.dump(img_corner_list, open(cache_path, "wb"))

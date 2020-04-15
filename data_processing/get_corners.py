f645cf06d6bc73aa22fc8627598f92e9ddaead9e
Soumya Vadlamannati <vadlamannati1997@gmail.com>
2020-04-14 21:02:01 -0400

    Get corners

---
+++ data_processing/get_corners.py
@@ -0,0 +1,32 @@
+import numpy as np
+from tqdm import tqdm
+from scipy.io import loadmat
+
+from config import cfg
+
+data_root = '../data/SUNRGBDMeta3DBB_v2.mat'
+
+def get_stats():
+	corners_list = []
+	data = loadmat(data_root)['SUNRGBDMeta'].squeeze()
+	for scene in tqdm(data):
+		objects = scene[10]
+		num_objs = objects.shape[1]
+		if num_objs == 0 or num_objs > cfg['MAX_NUM'] or num_objs < cfg['MIN_NUM']:
+			continue
+		objects = objects.squeeze(0) # num_objs
+		for obj in objects:
+			basis = obj[0] * obj[1].T
+			corner_1 = obj[2] + basis[0] / 2 + basis[1] / 2
+			corner_2 = obj[2] - basis[0] / 2 + basis[1] / 2
+			corner_3 = obj[2] - basis[0] / 2 - basis[1] / 2
+			corner_4 = obj[2] + basis[0] / 2 - basis[1] / 2
+			corners = np.vstack([corner_1, corner_2, corner_3, corner_4])
+			corners_list.append(corners)
+	
+	corners_list = np.stack(corners_list)
+	return corners_list
+
+	
+if __name__ == '__main__':
+	get_stats()

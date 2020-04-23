7b5c8ff2bc34211886d3817dd0ed5c733aa40147
Ritwick <rchaudhr@andrew.cmu.edu>
2020-04-23 00:26:08 -0400

    Adding randomized rotation transforms

--- data/transforms.py
+++ data/transforms.py
@@ -2,6 +2,7 @@ import torch
 import torchvision
 import numpy as np
 import torch.nn.functional as F
+from scipy.ndimage import rotate
 
 class RandomFlip(object):
 	"""
@@ -40,65 +41,29 @@ class RandomFlip(object):
 		return image
 
 
-class Rotate(object):
+class RandomRotation(object):
 	"""
-		Flipping the map image
+		Rotating the map image, randomly
 		Args:
-			direction: 'horizontal' or 'vertical
+			min: Minimum Angle to rotate by
+			max: Maximum Angle to rotate by
 	"""
 
-	def __init__(self, direction='horizontal'):
-		self.direction = direction
-		assert self.is_valid_direction(), "Invalid direction"
-		self.fun = self.direction_to_flip_fun()
+	def __init__(self, min_angle=-20, max_angle=20):
+		self.min = min_angle
+		self.max = max_angle
+		self.is_valid_angle()
 
-	def is_valid_direction(self):
-		return self.direction in ['horizontal', 'vertical']
+	def is_valid_angle(self):
+		assert self.max >= self.min
+		assert self.min >= -180
+		assert self.max <= 180
 	
-	def direction_to_flip_fun(self):
-		if self.direction == 'horizontal':
-			fun = np.fliplr
-		elif self.direction == 'vertical':
-			fun = np.flipud
-		else:
-			assert False, "Wrong direction"
-		return fun
-	
-	def __call__(self, image):
-		image = image.transpose((1,2,0))
-		image = self.fun(image)
-		return image.transpose((2,0,1))
-
-class MultipleRandomCrops(object):
-	"""
-	Multiple Random Crops.
-	Args:
-		outputSize (tuple or int): Desired output size. If int, square crop is made.
-		numCrops: Number of Random Crops        
-	"""
-
-	def __init__(self, outputSize, numCrops, scaleSize):
-		assert isinstance(outputSize, (int, tuple))
-		if isinstance(outputSize, int):
-			self.outputSize = (outputSize, outputSize)
-		else:
-			assert len(outputSize) == 2
-			self.outputSize = outputSize
-		assert isinstance(numCrops, int)
-		self.numCrops = numCrops
-		assert isinstance(scaleSize, int)
-		self.scaleSize = scaleSize
+	def sample_angle(self):
+		angle = np.random.random() * (self.max - self.min) + self.min
+		return angle
 
 	def __call__(self, image):
-		# NOTE: Assumed - Image Shape - (C,H,W)
-		dtype, device = image.dtype, image.device
-		image = image.numpy()
-		H, W = image.shape[1:]
-		randomCrops = np.zeros(((self.numCrops,image.shape[0]) + self.outputSize), dtype=image.dtype)
-		for i in range(self.numCrops):
-			top = np.random.randint(0, H - self.outputSize[0])
-			left = np.random.randint(0, W - self.outputSize[1])
-
-			randomCrops[i,...] = image[:, top : top + self.outputSize[0],
-									left : left + self.outputSize[0]]
-		return F.interpolate(torch.tensor(randomCrops, dtype=dtype, device=device), size=self.scaleSize)
+		angle = self.sample_angle()
+		image = rotate(image, angle, axes=(1,2), mode='constant', cval=0.0, order=0)
+		return image

1cb4a0427c147cfb7a6a3dd2c2181d78a19939bc
Ritwick <rchaudhr@andrew.cmu.edu>
2020-05-01 04:45:39 -0400

    Added super efficient translation code for neighbours using geometry.

---
+++ src/generator.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+from utils import get_extents_of_box, convert_to_dim
+
+class Generator():
+	def __init__(self):
+		super().__init__()
+
+	def get_translation_extent(self, all_corners, areas, extents, dim, object_idx):
+		assert dim == 0 or dim == 1,  "Only for x and y"
+		
+		X_MIN, X_MAX, Y_MIN, Y_MAX = extents
+		OTHER_DIM_MIN, OTHER_DIM_MAX, DIM_MIN, DIM_MAX = convert_to_dim(*extents, dim)
+
+		other_dim = 1-dim
+		# Get the MBR of the current object
+		curr_object_box = all_corners[object_idx] # 4 x 3
+		min_x, max_x, min_y, max_y = get_extents_of_box(curr_object_box)
+		# Transform to dim space
+		curr_min_other_dim, curr_max_other_dim, curr_min_dim, curr_max_dim = convert_to_dim(min_x, max_x, min_y, max_y, dim)
+		# Filter the boxes to get the target boxes
+		indices = np.ones(all_corners.shape[0], dtype=bool)
+		indices[object_idx] = False
+		other_boxes = all_corners[indices]
+		other_boxes_max_other_dim = other_boxes[:,:,other_dim].max(axis=1) # N 
+		other_boxes_min_other_dim = other_boxes[:,:,other_dim].min(axis=1) # N
+		target_boxes = other_boxes[~np.logical_or(other_boxes_max_other_dim <= curr_min_other_dim, 
+									other_boxes_min_other_dim >= curr_max_other_dim)]
+		# Segregate before and after other boxes.
+		# Enforce wall checks and also empty before/after
+		if target_boxes.shape[0] == 0:
+			before_box_indices = None
+			after_box_indices = None
+		else:
+			before_box_indices = target_boxes[:,:,dim].max(axis=1) <= curr_min_dim
+			if before_box_indices.sum() == 0:
+				before_box_indices = None
+			after_box_indices = target_boxes[:,:,dim].min(axis=1) >= curr_min_dim
+			if after_box_indices.sum() == 0:
+				after_box_indices = None
+		# Get the extents
+		before_extent = target_boxes[before_box_indices,:,dim].max() if before_box_indices is not None else DIM_MIN
+		after_extent = target_boxes[after_box_indices,:,dim].min() if after_box_indices is not None else DIM_MAX
+		return (curr_min_dim - before_extent, after_extent - curr_max_dim)
+
+	def next(self, all_corners, areas, extents):
+		all_new_corners = []
+		boxes = []
+		X_MIN, X_MAX, Y_MIN, Y_MAX = extents
+
+
+if __name__ == '__main__':
+	from test import *
+	generator = Generator()
+	print(generator.get_translation_extent(all_corners1, None, extents1, 1, index1))
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,19 @@
+import numpy as np
+
+def get_extents_of_box(box):
+    # assert box.shape == (4,3)
+    xs = box[:,0]
+    ys = box[:,1]
+    return xs.min(), xs.max(), ys.min(), ys.max()
+
+def convert_to_dim(min_x, max_x, min_y, max_y, dim):
+    assert dim in [0,1], "Wrong dim"
+    if dim == 0:
+        return (min_y, max_y, min_x, max_x)
+    else:
+        return (min_x, max_x, min_y, max_y)
+
+
 class AvgMeter():
     def __init__(self):
         self.val = 0.0
